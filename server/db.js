b139751e3e407c54d1689ea947d65623295104a0
Creator11 <trohin.danil2015@yandex.ru>
2022-01-08 18:25:07 +0300

    fixed server bugs, now server works

--- server/db.js
+++ server/db.js
@@ -1,12 +1,21 @@
+
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+
+
+module.exports = new Sequelize({
+    database: " ",
+    username: " ",
+    password: " ",
+    host: " ",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false  
+      }
+    },
+  });
+    
+
--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+
 app.use(fileUpload({}))
 app.use('/api', router)
 
@@ -25,6 +25,7 @@ const start = async () => {
         await sequelize.authenticate()
         await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        
     } catch (e) {
         console.log(e)
     }
